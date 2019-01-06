a1e0e833a9c31f0cb3c86e34d1a74b62f63151f4
Cvolton <clubpenguinmisa@gmail.com>
2019-01-06 06:47:23 +0100

    more work done on homework/Úkoly
    it now parses the xml succesfully but it fails to render it in the actual page

--- app/src/main/java/cz/michaelbrabec/fossbakalari/MainActivity.java
+++ app/src/main/java/cz/michaelbrabec/fossbakalari/MainActivity.java
@@ -1,28 +1,40 @@
 package cz.michaelbrabec.fossbakalari;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private Handler mHandler = new Handler();
+    private static final int drawerDelay = 200;
     SharedPrefHandler sharedPrefHandler = new SharedPrefHandler();
     String bakalariUrl;
     String token;
+    Boolean skipInit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +42,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -49,21 +52,31 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        token = sharedPrefHandler.getString(this, "tokenBase");
-        String loginJmeno = sharedPrefHandler.getString(this, "loginJmeno");
-        String loginSkola = sharedPrefHandler.getString(this, "loginStrtyp");
-        bakalariUrl = sharedPrefHandler.getString(this, "bakalariUrl");
-        if(token.equals("") || loginJmeno.equals("") || loginSkola.equals("")){
-            Intent intent = new Intent(this, LoginScreen.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
+        if(!skipInit){
+            token = sharedPrefHandler.getString(this, "tokenBase");
+            String loginJmeno = sharedPrefHandler.getString(this, "loginJmeno");
+            String loginSkola = sharedPrefHandler.getString(this, "loginStrtyp");
+            bakalariUrl = sharedPrefHandler.getString(this, "bakalariUrl");
+            if(token.equals("") || loginJmeno.equals("") || loginSkola.equals("") || bakalariUrl.equals("")){
+                Intent intent = new Intent(this, LoginScreen.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+
+            try{
+                token = generateTokenFromBase(token);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+            TextView navJmeno = navigationView.getHeaderView(0).findViewById(R.id.loginJmeno);
+            TextView navSkola = navigationView.getHeaderView(0).findViewById(R.id.loginSkola);
+
+            navJmeno.setText(loginJmeno);
+            navSkola.setText(loginSkola);
+
+            skipInit = true;
         }
 
-        TextView navJmeno = navigationView.getHeaderView(0).findViewById(R.id.loginJmeno);
-        TextView navSkola = navigationView.getHeaderView(0).findViewById(R.id.loginSkola);
-
-        navJmeno.setText(loginJmeno);
-        navSkola.setText(loginSkola);
     }
 
     @Override
@@ -103,24 +116,77 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawers();
+        drawer.closeDrawer(GravityCompat.START);
 
         int id = item.getItemId();
 
         if (id == R.id.nav_absence) {
 
         } else if (id == R.id.nav_rozvrh) {
-            Intent intent = new Intent(getApplicationContext(), RozvrhActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            startActivity(intent);
-            this.overridePendingTransition(0, 0);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), RozvrhActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                }
+            }, drawerDelay);
+
         } else if (id == R.id.nav_ukoly) {
-            Intent intent = new Intent(getApplicationContext(), UkolyActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-            startActivity(intent);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getApplicationContext(), UkolyActivity.class).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                }
+            }, drawerDelay);
         } else if (id == R.id.nav_znamky) {
 
         }
 
         //drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    //TODO: merge duplicate methods from here and LoginScreen (getWebContent, generateTokenFromBase, getSha512)
+    public String getWebContent(URL url){
+        try{
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String content = "", line;
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+            return content;
+        }catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static String generateTokenFromBase(String tokenBase) throws NoSuchAlgorithmException {
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat mdformat = new SimpleDateFormat("YYYYMMDD");
+        String strDate = mdformat.format(calendar.getTime());
+        String token = getSha512(tokenBase + strDate);
+        token = token.replace("/", "_");
+        token = token.replace("+", "-");
+        return token;
+    }
+
+    public static String getSha512(String hashPasswd) throws NoSuchAlgorithmException{
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        md.update(hashPasswd.getBytes());
+        byte[] bytes = md.digest();
+        return Base64.encodeToString(bytes, Base64.NO_WRAP);
     }
 }
--- app/src/main/java/cz/michaelbrabec/fossbakalari/UkolyActivity.java
+++ app/src/main/java/cz/michaelbrabec/fossbakalari/UkolyActivity.java
@@ -1,10 +1,55 @@
 package cz.michaelbrabec.fossbakalari;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+
 
 public class UkolyActivity extends MainActivity {
+
+    private class GetUkolyTask extends AsyncTask<String, Integer, String> {
+        protected String doInBackground(String... urls) {
+            try{
+                URL url = new URL(urls[0]);
+                return getWebContent(url);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+        }
+
+        protected void onPostExecute(String result) {
+            updateUkolyList(result);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +61,127 @@ public class UkolyActivity extends MainActivity {
         navView.getMenu().getItem(0).setChecked(true);
 
         setTitle(R.string.nav_item_ukoly); //workaround so we can have the name hardcoded to Fossaláři, so our app doesn't get renamed to Úkoly
+
+        new GetUkolyTask().execute(bakalariUrl + "/login.aspx?hx="+token+"&pm=ukoly");
+    }
+
+    public void updateUkolyList(String ukoly)
+
+    {
+        TextView temporary = findViewById(R.id.textView12);
+        //temporary.setText(ukoly);
+        XmlPullParserFactory parserFactory;
+        try {
+            parserFactory = XmlPullParserFactory.newInstance();
+            XmlPullParser parser = parserFactory.newPullParser();
+            InputStream is = new ByteArrayInputStream(ukoly.getBytes("UTF-8"));
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(is, null);
+
+            int eventType = parser.getEventType();
+
+
+            String tagName = "", tagContent = "", predmet = "", popis = "";
+            int event = parser.getEventType();
+
+            while (event != XmlPullParser.END_DOCUMENT) {
+                tagName = parser.getName();
+
+                switch (event) {
+                    case XmlPullParser.START_TAG:
+                        break;
+
+                    case XmlPullParser.TEXT:
+                        tagContent = parser.getText();
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        switch(tagName) {
+                            case "predmet": predmet = tagContent;
+                                break;
+                            case "popis": popis = tagContent;
+                                break;
+                            case "status": renderUkol(predmet, popis, tagContent);
+                                break;
+                        }
+                        break;
+                }
+
+                event = parser.next();
+            }
+
+            /*while (eventType != XmlPullParser.END_DOCUMENT) {
+                String eltName = null;
+
+                switch (eventType) {
+                    case XmlPullParser.START_TAG:
+                        String predmet = parser.nextText();
+                        String popis = parser.nextText();
+                        String status = parser.nextText();
+
+                        Log.d("updateUkolyList", predmet + ";" + popis + ";" + status);
+
+                        renderUkol(predmet, popis, status);
+                        break;
+                }
+
+                eventType = parser.next();
+            }*/
+
+        } catch (XmlPullParserException e) {
+
+        } catch (IOException e) {
+        }
+    }
+
+    public void renderUkol(String predmet, String popis, String status){
+        Log.d("renderUkol", predmet + ";" + popis + ";" + status);
+
+        //very top linearlayout (not including this causes it to fall apart for some reason)
+        LinearLayout parent = findViewById(R.id.topParentUkoly);
+
+        //linearlayout with checkbox
+
+        LinearLayout layoutWithCheckbox = new LinearLayout(this);
+
+        layoutWithCheckbox.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1.0f));
+        layoutWithCheckbox.setOrientation(LinearLayout.HORIZONTAL);
+
+        parent.addView(layoutWithCheckbox);
+
+        //the checkbox itself
+        CheckBox checkBox = new CheckBox(this);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = 10;
+
+        layoutWithCheckbox.addView(checkBox, params);
+
+        //another nested layout for textviews
+
+        LinearLayout layoutWithTextViews = new LinearLayout(this);
+
+        layoutWithTextViews.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layoutWithTextViews.setOrientation(LinearLayout.VERTICAL);
+
+        //textviews
+        TextView predmetView = new TextView(this);
+        predmetView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16f);
+        predmetView.setTextColor(0);
+        predmetView.setText(predmet);
+        params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = 10;
+        layoutWithCheckbox.addView(predmetView, params);
+
+        TextView popisView = new TextView(this);
+        params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.leftMargin = 10;
+        layoutWithCheckbox.addView(popisView, params);
+
+        View divider = new View(this);
+        params = new LinearLayout.LayoutParams(1, 10);
+        params.setMargins(10,10,10,10);
+        divider.setBackgroundColor(000);
+        parent.addView(divider);
     }
 }
